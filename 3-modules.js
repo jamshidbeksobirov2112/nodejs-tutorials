a6305df7c8236593d48fe36a10cc257a704ac1a5
jamshidbeksobirov2112 <116828790+jamshidbeksobirov2112@users.noreply.github.com>
2022-11-12 22:21:52 +0500

    Add files via upload

---
+++ 3-modules.js
@@ -0,0 +1,7 @@
+const sayHi = require('./5-utils')
+const names = require('./4-names');
+const data = require('./6-alternative-flavor')
+require('./7-mind-grenade')
+
+sayHi(names.john)
+sayHi(names.peter)
